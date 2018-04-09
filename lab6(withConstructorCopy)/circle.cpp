79ce340607408e464a6cac73f379f9f67b5cc698
ilyatim <31735653+ilyatim@users.noreply.github.com>
2018-04-09 13:28:33 +0300

    Update circle.cpp

--- lab6(withConstructorCopy)/circle.cpp
+++ lab6(withConstructorCopy)/circle.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tuple>
 #include "circle.h"
 
 using namespace std;
@@ -7,18 +8,17 @@ Circle::Circle()
   cout << "+Respect(Circle)" << endl;
 }
 
-Circle::Circle(tuple <int, int, int> Circle)
+Circle::Circle(int point1, int point2, int point3)
 {
-  cout << "enter the circle center coordinate and his radius" << endl;
-  cin >> get <0> (Circle);
-  get<0>(pCircle) = get<0>(Circle);
-  cin >> get <1> (Circle);
-  get<1>(pCircle) = get<1>(Circle);
-  cin >> get <2> (Circle);
-  get<2>(pCircle) = get<2>(Circle);
+  auto MyCircle = make_tuple(point1, point2, point3);
+  pCircle = MyCircle;
   cout << "+Respect(Circle)" << endl;
 }
-
+Circle::Circle(const Circle &other)
+{
+    pCircle = other.pCircle;
+    std::cout << "=Respect(Circle)" << std::endl;
+}
 Circle::~Circle()
 {
   cout << "-Respect(Circle)" << endl;
