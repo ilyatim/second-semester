6a598e70a78acfb2d4568c7cd9b03fde6516c33b
ilyatim <31735653+ilyatim@users.noreply.github.com>
2018-04-09 13:32:41 +0300

    Update poligon.cpp

--- lab6(withConstructorCopy)/poligon.cpp
+++ lab6(withConstructorCopy)/poligon.cpp
@@ -8,32 +8,16 @@ Poligon::Poligon()
 }
 Poligon::Poligon(Point a, Point b, Point c, Point d)
 {
-  cout << "set 4 coordinate" << endl;
-  cout << "1st coordinate" << endl;
-  cin >> a.first >> a.second;
   A = a;
-  cout << "2nd coordinate" << endl;
-  cin >> b.first >> b.second;
   B = b;
-  cout << "3rd coordinate" << endl;
-  cin >> c.first >> c.second;
   C = c;
-  cout << "4th coordinate" << endl;
-  cin >> d.first >> d.second;
   D = d;
   cout << "+Respect(Poligon)" << endl;
 }
 Poligon::Poligon(Point a, Point b, Point c)
 {
-  cout << "set 3 coordinate" << endl;
-  cout << "1st coordinate" << endl;
-  cin >> a.first >> a.second;
   A = a;
-  cout << "2nd coordinate" << endl;
-  cin >> b.first >> b.second;
   B = b;
-  cout << "3rd coordinate" << endl;
-  cin >> c.first >> c.second;
   C = c;
   cout << "+Respect(poligon)" << endl;
 }
